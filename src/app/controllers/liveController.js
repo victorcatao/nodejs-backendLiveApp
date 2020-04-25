c6051d237f03622fa3ef812a68f23c252bcd86a5
Victor Catão <victorcatao@MacBook-Pro-de-Victor.local>
2020-04-25 14:46:13 -0300

    test

--- src/app/controllers/liveController.js
+++ src/app/controllers/liveController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const firebase = require('../helpers/firebase')
-const Live = require('../models/Live');
+const Live = require('../models/live');
 
 router.post('/createLive', function(req, res) {
 
