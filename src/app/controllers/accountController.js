869da20bd55d8b779f32bca60a8759cb4f819361
Victor Catão <victorcatao@MacBook-Pro-de-Victor.local>
2020-04-25 14:37:24 -0300

    test

--- src/app/controllers/accountController.js
+++ src/app/controllers/accountController.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/auth');
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 
-const User = require('../models/User');
+// const User = require('../models/User');
 
 const router = express.Router();
 
@@ -10,76 +10,76 @@ const router = express.Router();
 router.use(authMiddleware);
 
 
-router.put('/updatePassword', async (req, res) => {
+// router.put('/updatePassword', async (req, res) => {
 
-  req.body.password = await bcrypt.hash(req.body.password, 10);
+//   req.body.password = await bcrypt.hash(req.body.password, 10);
 
-  const user = await User.findOneAndUpdate(
-    { _id: req.userId },
-    { $set: { password: req.body.password } },
-    {upsert: true, new: true}
-  ).exec();
+//   const user = await User.findOneAndUpdate(
+//     { _id: req.userId },
+//     { $set: { password: req.body.password } },
+//     {upsert: true, new: true}
+//   ).exec();
 
-  if(user) {
-    res.send(user)
-  } else {
-    res.status(400).send({ error: 'Something went wrong' })
-  }
+//   if(user) {
+//     res.send(user)
+//   } else {
+//     res.status(400).send({ error: 'Something went wrong' })
+//   }
 
-});
-
-
-router.get('/updatePhoto', (req, res) => {
-  res.send({ok: true, user_id: req.userId});
-});
+// });
 
 
-router.put('/updateAbout', (req, res) => {
-
-  User.findOneAndUpdate(
-    { _id: req.userId },
-    { $set: req.body },
-    { new: true },
-    function(err, doc){
-      if(err){
-        res.status(400).send({ error: 'Something went wrong' })
-      }
-      res.send(doc)
-  });
-
-});
-
-router.get('/getAbout', async (req, res) => {
-  const user = await User.findById(req.userId);
-  res.send(user);
-});
+// router.get('/updatePhoto', (req, res) => {
+//   res.send({ok: true, user_id: req.userId});
+// });
 
 
-router.post('/registerToken', (req, res) => {
+// router.put('/updateAbout', (req, res) => {
 
-  User.findOneAndUpdate(
-    { _id: req.userId },
-    { $set: {
-        firebase_token: req.body.firebase_token,
-        device: {
-          model: req.body.model,
-          os: req.body.os,
-        },
-        app: {
-          build: req.body.build,
-          version: req.body.version
-        }
-      }
-    },
-    { new: true },
-    function(err, doc){
-      if(err){
-        res.status(400).send({ error: 'Something went wrong' })
-      }
-      res.send(doc)
-  });
+//   User.findOneAndUpdate(
+//     { _id: req.userId },
+//     { $set: req.body },
+//     { new: true },
+//     function(err, doc){
+//       if(err){
+//         res.status(400).send({ error: 'Something went wrong' })
+//       }
+//       res.send(doc)
+//   });
 
-});
+// });
+
+// router.get('/getAbout', async (req, res) => {
+//   const user = await User.findById(req.userId);
+//   res.send(user);
+// });
+
+
+// router.post('/registerToken', (req, res) => {
+
+//   User.findOneAndUpdate(
+//     { _id: req.userId },
+//     { $set: {
+//         firebase_token: req.body.firebase_token,
+//         device: {
+//           model: req.body.model,
+//           os: req.body.os,
+//         },
+//         app: {
+//           build: req.body.build,
+//           version: req.body.version
+//         }
+//       }
+//     },
+//     { new: true },
+//     function(err, doc){
+//       if(err){
+//         res.status(400).send({ error: 'Something went wrong' })
+//       }
+//       res.send(doc)
+//   });
+
+// });
 
 
 module.exports = app => app.use('/account', router);
