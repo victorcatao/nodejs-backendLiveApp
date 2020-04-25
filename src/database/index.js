d74f66eead2ca09568f8161b11bce4c5deadaa2e
Victor Catão <victorcatao@MacBook-Pro-de-Victor.local>
2020-04-25 15:16:47 -0300

    test

--- src/database/index.js
+++ src/database/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/noderest');
 // mongoose.connect('mongodb://victorcatao:Times993356@ds045907.mlab.com:45907/liveappdb');
 
+
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
