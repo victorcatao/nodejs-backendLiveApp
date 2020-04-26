7c7d5292e74cf251ebe2d63cbc07cb8834c18b70
Victor Catão <victorcatao@MacBook-Pro-de-Victor.local>
2020-04-26 18:34:30 -0300

    sendSuggestion

---
+++ src/app/models/suggestion.js
@@ -0,0 +1,25 @@
+const mongoose = require('../../database/index.js');
+
+const SuggestionSchema = new mongoose.Schema({
+  artist: {
+    type: String,
+    required: 'Preencha o artist'
+  },
+  social_network: {
+    type: String,
+    required: 'Preencha o social_network'
+  }
+});
+
+SuggestionSchema.set('toJSON', {
+    virtuals: true,
+    transform: (doc, ret, options) => {
+        delete ret.__v;
+        ret.id = ret._id.toString();
+        delete ret._id;
+    },
+});
+
+const Suggestion = mongoose.model('Suggestion', SuggestionSchema);
+
+module.exports = Suggestion;
