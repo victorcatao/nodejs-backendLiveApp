4159a20b303b6c33cb062f8b7d4a297f2064e443
Victor Catão <victorcatao@MacBook-Pro-de-Victor.local>
2020-05-01 00:04:04 -0300

    fix

--- src/app/controllers/liveController.js
+++ src/app/controllers/liveController.js
@@ -60,12 +60,12 @@ router.post('/create', function(req, res) {
 router.post('/convertEverybody', function(req, res) {
 	Live.find().then(function(docs) {
     	docs.forEach(function(live, index) {
-    		console.log(live.date + ' ' + live.time)
-    		var brasil    = moment.tz("28/04/2020 20:00", "America/Sao_Paulo");
+    		console.log(`${live.date} ${live.time}`)
+    		var brasil = moment(`${live.date} ${live.time}`, "DD/MM/YYYY HH:mm");
 			var utc = brasil.clone().tz("UTC");
 			console.log(utc.format())
-			
-    		console.log(moment(`${live.date} ${live.time}`, "DD-MM-YYYY HH:mm").tz("America/Sao_Paulo").clone().tz("UTC").format())
+
+    		// console.log(moment(`${live.date} ${live.time}`, "DD-MM-YYYY HH:mm").tz("America/Sao_Paulo").clone().tz("UTC").format())
     		// isso aqui funcionou
     		// console.log(moment(`${live.date} ${live.time}`, "DD-MM-YYYY HH:mm").tz("UTC").add(3, 'hours').format())
 
