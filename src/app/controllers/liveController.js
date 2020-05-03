3a47aa24ff76a8d8a4626b39dbe7ad600180b116
Victor Catão <victorcatao@MacBook-Pro-de-Victor.local>
2020-05-02 23:25:49 -0300

    fix

--- src/app/controllers/liveController.js
+++ src/app/controllers/liveController.js
@@ -147,8 +147,12 @@ router.get('/tomorrow', function(req, res) {
 
 router.get('/today', function(req, res) {
 
-	const startToday = moment().tz("UTC").startOf('day').add(3, 'hours').add(1, 'seconds').format()
-	const endToday = moment().tz("UTC").endOf('day').add(3, 'hours').format()
+	const startToday = moment().tz("UTC").subtract(3, 'hours').startOf('day').add(3, 'hours').format()
+	console.log(startToday)
+	const endToday = moment().tz("UTC").subtract(3, 'hours').endOf('day').add(3, 'hours').format()
+	console.log(endToday)
+	// const startToday = moment().tz("UTC").startOf('day').add(3, 'hours').add(1, 'seconds').format()
+	// const endToday = moment().tz("UTC").endOf('day').add(3, 'hours').format()
 	
 	const findRecord = (req.query.findRecord == 'true') || (req.query.findRecord == true)
   	const jsonFind = { 
