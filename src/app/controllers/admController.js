ba0fd9b4cbcf712e3d1e566ec187b556b1e3fa17
Victor Catão <victorcatao@MacBook-Pro-de-Victor.local>
2020-05-17 20:24:30 -0300

    Refactor

--- src/app/controllers/admController.js
+++ src/app/controllers/admController.js
@@ -6,6 +6,7 @@ const Suggestion = require('../models/suggestion')
 const mongoose = require('mongoose')
 const timeHelper = require('../helpers/timeHelper')
 const pushHelper = require('../helpers/pushHelper')
+const liveHelper = require('../helpers/liveHelper')
 const firebase = require('../helpers/firebase')
 const responseHelper = require('../helpers/responseHelper')
 
@@ -24,7 +25,7 @@ router.get('/getAllLives', async (req, res) => {
   		},
   		function(err, docs){
   			docs.forEach(function(live, index){
-  				setLiveIsLiveNow(live)
+  				liveHelper.setLiveIsLiveNow(live)
   			})
     		res.send(docs)
   		});
