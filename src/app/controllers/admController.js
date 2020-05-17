41802dd3432dd6a0c39b3ac50cd57c0450304bc3
Victor Catão <victorcatao@MacBook-Pro-de-Victor.local>
2020-05-17 20:29:28 -0300

    refactor

--- src/app/controllers/admController.js
+++ src/app/controllers/admController.js
@@ -8,7 +8,7 @@ const timeHelper = require('../helpers/timeHelper')
 const pushHelper = require('../helpers/pushHelper')
 const liveHelper = require('../helpers/liveHelper')
 const firebase = require('../helpers/firebase')
-const responseHelper = require('../helpers/responseHelper')
+const responseHelper = require('../helpers/responseHelper.js')
 
 const router = express.Router();
 
